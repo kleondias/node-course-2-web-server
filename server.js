3965fe6b8261cd316f9dddebb146a5fc4c42c343
Kleon Dias <kdias76@hotmail.com>
2019-05-24 22:11:21 +0300

    Add Project Page

--- server.js
+++ server.js
@@ -52,6 +52,12 @@ app.get('/about', (req,res)=>{
 	});
 });
 
+app.get('/projects', (req,res)=>{
+	res.render('projects',{
+		pageTitle: 'Projects',
+	});
+});
+
 app.get('/bad', (req,res)=>{
 	res.send({
 		error: 'Error handling request'
